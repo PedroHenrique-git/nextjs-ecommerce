61fb722460f06282c1cf350eb72933432c35bd55
PedroHenrique-git <pedromf34@outlook.com>
2023-04-01 22:36:43 -0300

    feat: sync pagination with window href

--- src/sdk/pagination/usePagination.ts
+++ src/sdk/pagination/usePagination.ts
@@ -1,4 +1,6 @@
-import { useReducer } from 'react';
+import { useRouter } from 'next/router';
+import { useReducer, useRef } from 'react';
+import { useIsomorphicLayoutEffect, useLocation } from 'react-use';
 
 interface State {
   nextPage: string | null;
@@ -28,7 +30,10 @@ export type Action =
       payload: {
         prevPageUrl: string | null;
       };
-    };
+    }
+  | { type: 'set_page'; payload: { page: string | null } }
+  | { type: 'set_take'; payload: { take: string | null } }
+  | { type: 'set_sort'; payload: { sort: string | null } };
 
 function reducer(state: State, action: Action): State {
   switch (action.type) {
@@ -64,13 +69,58 @@ function reducer(state: State, action: Action): State {
         ...(page ? { page } : {}),
       };
     }
+    case 'set_page': {
+      return {
+        ...state,
+        page: action.payload.page ?? '1',
+      };
+    }
+    case 'set_sort': {
+      return {
+        ...state,
+        sort: (action.payload.sort ?? 'asc') as 'asc' | 'desc',
+      };
+    }
+    case 'set_take': {
+      return {
+        ...state,
+        take: action.payload.take ?? '5',
+      };
+    }
     default:
       return state;
   }
 }
 
 function usePagination() {
+  const firstRender = useRef(true);
+  const location = useLocation();
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { push, pathname } = useRouter();
+
+  useIsomorphicLayoutEffect(() => {
+    if (firstRender.current) {
+      if (!location.href) return;
+
+      const windowURL = new URL(location.href);
+
+      const take = windowURL.searchParams.get('take');
+      const page = windowURL.searchParams.get('page');
+      const sort = windowURL.searchParams.get('sort');
+
+      take && dispatch({ type: 'set_take', payload: { take } });
+      page && dispatch({ type: 'set_page', payload: { page } });
+      sort && dispatch({ type: 'set_sort', payload: { sort } });
+
+      firstRender.current = false;
+    }
+
+    const { page, sort, take } = state;
+
+    push({ pathname, query: { page, sort, take } }, undefined, {
+      shallow: true,
+    });
+  }, [state]);
 
   return {
     state,
